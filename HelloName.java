eb970ee1184153190fedffe7babd2e951643a1ed
YutoOikawa <130010409+YutoOikawa@users.noreply.github.com>
2023-04-09 16:12:55 +0900

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,7 @@
+class HelloName{
+    public static void main(String args[]){
+        String name = "oikawa";
+        String age = "29";
+        System.out.println("こんにちは、私の名前は" + name + "年は" + age + "です");
+    }
+}
